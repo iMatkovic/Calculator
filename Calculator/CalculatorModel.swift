0dfff31a89f6cb917c02a87bcd1ae2478c143187
COBE <imatkovic92@gmail.com>
2016-10-05 15:24:03 +0200

    Initial

---
+++ Calculator/CalculatorModel.swift
@@ -0,0 +1,28 @@
+//
+//  CalculatorModel.swift
+//  Calculator
+//
+//  Created by COBE on 05/10/16.
+//  Copyright © 2016 COBE. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorModel{
+    
+    func setOpreand(operand: Double){}
+    
+    
+    func preformOperation(symbol: String){}
+    
+    
+    var result: Double{
+        get{
+            return 0.0
+        }
+    }
+    
+    
+    
+    
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton){
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+            
+        }else{
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    @IBAction func preformOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle{
+            if mathematicalSymbol == "π"{
+                displayValue = M_PI
+            }else if mathematicalSymbol == "√" {
+                displayValue = sqrt(displayValue)
+            }
+        }
+        
+    }
 }
-
